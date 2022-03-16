fd1945ac7bfa9893758baa9ff2cc09a593ee21a9
Ismael Mammadzada <mamedzade030@gmail.com>
2022-03-16 23:47:17 +0400

    ProductDetail class is createt. This is data transfer object for all three scraper implementations

---
+++ ScrapeLogic/ProductDetail.cs
@@ -0,0 +1,15 @@
+namespace ScrapeLogic;
+
+public class ProductDetail{
+    public ProductDetail(string title, string url, double price)
+    {
+        Title = title;
+        Url = url;
+        Price = price;
+    }
+
+    public string Title{get; set;}
+    public string Url{ get; set;}
+    public string Description{get; set;}
+    public double Price {get;set;}
+}
