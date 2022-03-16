0743675b2877b67e9406bf64b64eb0322002bfda
Ismael Mammadzada <mamedzade030@gmail.com>
2022-03-17 00:31:52 +0400

    Tap.az parser implemented. AuthenticatedUser, which is a impelmentation of Client abstraction is created. Filter method changed from abstract to normal method in AbstractScraper class

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ScrapeLogic;
+
+
+AbstractScraper scraper = new TapAzScraper();
+Client user = new AuthenticatedUser(scraper);
+List<ProductDetail> result = user.Search("divan");
+
+foreach(ProductDetail detail in result){
+    Console.WriteLine($"{detail.Title} - {detail.Price}");
+}
+
+List<ProductDetail> filtered = user.Filter<double>(p => p.Price);
+Console.WriteLine("After filtering by price: ");
+
+foreach(ProductDetail detail in filtered){
+    Console.WriteLine($"{detail.Title} - {detail.Price}");
+}
